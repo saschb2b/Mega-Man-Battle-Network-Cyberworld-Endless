e5679c400d1ab1016eabddc3e37ba70c35acd04a
Sascha Becker <saschb2b@gmail.com>
2026-09-24 14:04:13 +0200

    Draw SpoutMan and TenguMan as guardians, second battle backgrounds

--- src/core/run.c
+++ src/core/run.c
@@ -20,25 +20,28 @@ void run_new(uint32_t seed) {
 	run.biome_order[4] = BIOME_GRAVEYARD;
 	run.biome_order[5] = BIOME_UNDERNET;
 	/* Each area's guardian, drawn from navis that suit it. */
-	static const uint8_t pools[BIOME_COUNT][3] = {
-		{ 12, 2, 5 },   /* Central: BlastMan, ElecMan, ChargeMan */
-		{ 13, 2, 1 },   /* Seaside: DiveMan, ElecMan, HeatMan */
-		{ 16, 15, 2 },  /* Sky: ElementMan, JudgeMan, ElecMan */
-		{ 7, 9, 5 },    /* Green: TomahawkMan, GroundMan, ChargeMan */
-		{ 4, 10, 3 },   /* Graveyard: EraseMan, DustMan, SlashMan */
-		{ 11, 3, 1 },   /* Undernet: ProtoMan, SlashMan, HeatMan */
-		{ 11, 11, 11 }, /* Secret Area: ProtoMan SP */
-		{ 3, 4, 11 },   /* Cybeast Nest */
-		{ 14, 12, 17 }, /* Comp: CircusMan, BlastMan, Colonel */
-		{ 14, 16, 13 }, /* Homepage: CircusMan, ElementMan, DiveMan */
-		{ 17, 15, 12 }, /* Comp (second): Colonel, JudgeMan, BlastMan */
+	static const uint8_t pools[BIOME_COUNT][4] = {
+		{ 12, 2, 5, 12 },   /* Central: BlastMan, ElecMan, ChargeMan */
+		{ 13, 6, 2, 1 },    /* Seaside: DiveMan, SpoutMan, ElecMan, HeatMan */
+		{ 16, 15, 8, 2 },   /* Sky: ElementMan, JudgeMan, TenguMan, ElecMan */
+		{ 7, 9, 5, 7 },     /* Green: TomahawkMan, GroundMan, ChargeMan */
+		{ 4, 10, 3, 4 },    /* Graveyard: EraseMan, DustMan, SlashMan */
+		{ 11, 3, 1, 11 },   /* Undernet: ProtoMan, SlashMan, HeatMan */
+		{ 11, 11, 11, 11 }, /* Secret Area: ProtoMan SP */
+		{ 3, 4, 11, 3 },    /* Cybeast Nest */
+		{ 14, 12, 17, 14 }, /* Comp: CircusMan, BlastMan, Colonel */
+		{ 14, 16, 13, 16 }, /* Homepage: CircusMan, ElementMan, DiveMan */
+		{ 17, 15, 12, 17 }, /* Comp (second): Colonel, JudgeMan, BlastMan */
 	};
-	for (int b = 0; b < BIOME_COUNT; ++b) run.boss_order[b] = pools[b][rng_range(0, 2)];
+	for (int b = 0; b < BIOME_COUNT; ++b) run.boss_order[b] = pools[b][rng_range(0, 3)];
 }
 
 int biome_bg(int b) {
-	/* the BattleSettings background byte (0x00-0x15) for each biome's battles */
-	static const int bg[BIOME_COUNT] = { 0x07, 0x0B, 0x04, 0x0D, 0x14, 0x0F, 0x13, 0x15, 0x06, 0x03, 0x08 };
-	return b >= 0 && b < BIOME_COUNT ? bg[b] : 0;
+	/* the BattleSettings background (0x00-0x15) of each area's battles, two
+	 * where the game has a second that suits it */
+	static const uint8_t bg[BIOME_COUNT][2] = {
+		{ 0x07, 0x09 }, { 0x0B, 0x0A }, { 0x04, 0x10 }, { 0x0D, 0x0C }, { 0x14, 0x12 }, { 0x0F, 0x11 },
+		{ 0x13, 0x13 }, { 0x15, 0x15 }, { 0x06, 0x06 }, { 0x03, 0x01 }, { 0x08, 0x00 },
+	};
+	return b >= 0 && b < BIOME_COUNT ? bg[b][rng_range(0, 1)] : 0;
 }
-
